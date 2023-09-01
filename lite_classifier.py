37e747fed73f43145f9718ff3f13b4312b0a769b
okba <33584599+okbach@users.noreply.github.com>
2023-09-01 09:50:17 -0700

    Update lite_classifier.py
    Small correction to accept image matrices

--- lite_classifier.py
+++ lite_classifier.py
@@ -2,8 +2,7 @@ import os
 import cv2
 import pydload
 import numpy as np
-
-from .image_utils import load_images
+from image_utils import load_images
 
 
 class LiteClassifier:
@@ -25,18 +24,23 @@ class LiteClassifier:
     def classify(self, image_paths, size=(256, 256)):
         if isinstance(image_paths, str):
             image_paths = [image_paths]
-
+        else : image_paths = [image_paths]
         result = {}
         for image_path in image_paths:
-            loaded_images, _ = load_images([image_path], size, image_names=[image_path])
+            #print (image_path.shape)
+            loaded_images, _ = load_images([image_path], size, image_names=['1'])
+            #print(loaded_images[0].shape)
             loaded_images = np.rollaxis(loaded_images, 3, 1)
+            #print(loaded_images[0].shape)
+            
 
             self.lite_model.setInput(loaded_images)
             pred = self.lite_model.forward()
 
-            result[image_path] = {
-                "unsafe": pred[0][0],
+            result['1'] = {
+                 "unsafe": pred[0][0],
                 "safe": pred[0][1],
             }
 
         return result
+
