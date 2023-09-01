73fd3fc65b25796f83196f3609649e1369d40038
okba <33584599+okbach@users.noreply.github.com>
2023-09-01 09:56:09 -0700

    Update main.py
    you can Display the captured screen using matplotlib
    
    use pyautogui  for 
    If a pornographic scene is detected . onkeypress arrow keys work in YouTube, pass 5 seconds

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 
 from ScreenCapture import ScreenCapture  
+from lite_classifier import LiteClassifier as NudeClassifierLite
+import matplotlib.pyplot as plt
+from PIL import Image as pil_image
+import pyautogui
 
 
+classifier_lite = NudeClassifierLite()
 
-x, y = 0, 0
-grabzone = 800
+capture = ScreenCapture()
 
-
-capture = ScreenCapture(x, y, grabzone)
-
+screen_width, screen_height= capture.get_actual_screen_dimensions()
 
 
 while True:
+    
     screen = capture.get_screen()
+    
+    # Display the captured screen using matplotlib - You slow down the process
+    '''
+    pil_screen = pil_image.fromarray(screen)
+    plt.imshow(pil_screen)
+    plt.axis('off')  # Turn off axis
+    plt.show(block=False)
+    plt.pause(1)
+    '''
 
+    unsafe = classifier_lite.classify(screen)["1"]["unsafe"]
+    safe = classifier_lite.classify(screen)["1"]["safe"]
+    print('safe : ' , safe )
+    print('unsafe : ' , unsafe)
+    if unsafe > 0.35 and safe < 0.7:
+        print('sex ')
+        pyautogui.press('right')
 
+        
 
+    #you can try by url img 
+    #print(classifier_lite.classify('C:/Users/deep_root/Desktop/a.jpg'))
+    
 
 
